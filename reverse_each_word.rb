30d7782042f17bbe7209974b035ad47464cd91db
Scott Espinosa <scottjames.espinosa@gmail.com>
2020-05-15 23:01:48 +0000

    Automatically backed up by Learn

---
+++ reverse_each_word.rb
@@ -0,0 +1,17 @@
+def reverse_each_word(string)
+  string_array = string.split(" ")
+  reverse_string_array = []
+  string_array.each do |element|
+    element_array = element.split("")
+    reverse_element_array = []
+    reverse_string = ""
+    count = element_array.length 
+    while count > 0 do 
+      reverse_element_array.push(element_array[count - 1])
+      count -= 1 
+    end 
+    reverse_string = reverse_element_array.join("")
+    reverse_string_array.push(reverse_string)
+  end 
+  reverse_string_array.join(" ")
+end
